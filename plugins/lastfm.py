1904ef6bb2b8c3834335c02674d3f95c9696c390
sentriz <senan.f.b.kelly+github@gmail.com>
2017-06-30 14:48:52 +0100

    fromat lastfm

--- plugins/lastfm.py
+++ plugins/lastfm.py
@@ -2,15 +2,22 @@ import requests
 from tinydb import TinyDB, Query
 from steelybot import config
 
+
 COMMAND = '.np'
 USERDB = TinyDB('../lastfm.json')
 USER = Query()
+
 
 def get_np(apikey, user):
     base = "http://ws.audioscrobbler.com/2.0/"
     url = "?method=user.getrecenttracks&user={}&api_key={}&limit=2&format=json".format(user, apikey)
     res = requests.get(base + url)
+    print(res)
+    print(res)
+    print(res)
+    print(res)
     return res.json()["recenttracks"]["track"][0]
+
 
 def extract_song(user):
     try:
@@ -23,22 +30,22 @@ def extract_song(user):
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    message = message.split()
+    message_split = message.split()
     if not message:
-        results = USERDB.search(USER.fb_id == author_id)
-        if len(results) != 0:
-            lastfm_name = results[0]['lastfm']
+        search = USERDB.search(user.fb_id == author_id)
+        if len(search) != 0:
+            lastfm_name = search[0]['lastfm']
             bot.sendMessage(extract_song(lastfm_name), thread_id=thread_id, thread_type=thread_type)
         else:
             bot.sendMessage('include username please or use .np set', thread_id=thread_id, thread_type=thread_type)
         return
 
-    elif message[0] == 'set' and len(message) == 2:
-        if len(USERDB.search(USER.fb_id == author_id)) == 0:
-            USERDB.insert({"fb_id" : author_id, "lastfm" : m[1]})
+    elif message_split[0] == 'set' and len(message_split) == 2:
+        if len(USERDB.search(user.fb_id == author_id)) == 0:
+            USERDB.insert({"fb_id" : author_id, "lastfm" : message_split[1]})
             bot.sendMessage('good egg', thread_id=thread_id, thread_type=thread_type)
         else:
-            USERDB.update({"lastfm" : m[1]}, USER.fb_id == author_id)
+            USERDB.update({"lastfm" : message_split[1]}, user.fb_id == author_id)
             bot.sendMessage('updated egg', thread_id=thread_id, thread_type=thread_type)
         return
     else:
