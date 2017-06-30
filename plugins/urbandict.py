f1f4810db3f11be008659ef44e1e46a887200698
sentriz <senan.f.b.kelly+github@gmail.com>
2017-06-30 18:16:56 +0100

    update urbandict

--- plugins/urbandict.py
+++ plugins/urbandict.py
@@ -1,22 +1,24 @@
 import requests
 
+
 COMMAND = '.ud'
+
 
 def define(term):
     results = []
     term = '+'.join(term.split())
-    r = requests.get('http://api.urbandictionary.com/v0/define?term=' + term)
-    if r.json()['result_type'] != 'exact':
+    response = requests.get('http://api.urbandictionary.com/v0/define?term=' + term)
+    if response.json()['result_type'] != 'exact':
         return results
-    for result in r.json()['list']:
-        results.append({'definition':result['definition'], 'example':result['example']})
+    for result in response.json()['list']:
+        results.append({'definition': result['definition'], 'example': result['example']})
     return results
+
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
     definitions = define(message)
     if not message or not definitions:
         bot.sendMessage('no results', thread_id=thread_id, thread_type=thread_type)
         return
-    text = 'Definition: ' + definitions[0]['definition'] + '\nExample: ' + definitions[0]['example']
+    text = '{definition}\n "{example}"'.format(**definitions[0])
     bot.sendMessage(text, thread_id=thread_id, thread_type=thread_type)
-    
