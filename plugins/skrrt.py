c9de6c0e5454004ef7adce964609624c2df26a18
alex <Alex McGrath Kraak>
2017-06-30 11:50:56 +0100

    make skrrt better

--- plugins/skrrt.py
+++ plugins/skrrt.py
@@ -2,4 +2,4 @@ from random import randint
 COMMAND = '.skrrt'
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    bot.sendMessage('skrrrt' * randint(1,3), thread_id=thread_id, thread_type=thread_type)
+    bot.sendMessage('sk{}t'.format('r'*randint(1,3)), thread_id=thread_id, thread_type=thread_type)
