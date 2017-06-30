ab928c00364b9b3c135119ec002bdfe4405f5787
sentriz <senan.f.b.kelly+github@gmail.com>
2017-06-30 02:18:48 +0100

    lower eight ball replies

--- plugins/eightball.py
+++ plugins/eightball.py
@@ -3,26 +3,26 @@ import random
 
 COMMAND = '.8'
 REPLIES = (
-    "It is certain",
-    "It is decidedly so",
-    "Without a doubt",
-    "Yes, definitely",
-    "You may rely on it",
-    "As I see it, yes",
-    "Most likely",
-    "Outlook good",
-    "Yes",
-    "Signs point to yes",
-    "Reply hazy, try again",
-    "Ask again later",
-    "Better not tell you now",
-    "Cannot predict now",
-    "Concentrate and ask again",
-    "Don\'t count on it",
-    "My reply is no",
-    "My sources say no",
-    "Outlook not so good",
-    "Very doubtful"
+    "it is certain",
+    "it is decidedly so",
+    "without a doubt",
+    "yes, definitely",
+    "you may rely on it",
+    "as i see it, yes",
+    "most likely",
+    "outlook good",
+    "yes",
+    "signs point to yes",
+    "reply hazy, try again",
+    "ask again later",
+    "better not tell you now",
+    "cannot predict now",
+    "concentrate and ask again",
+    "don\'t count on it",
+    "my reply is no",
+    "my sources say no",
+    "outlook not so good",
+    "very doubtful"
 )
 
 
