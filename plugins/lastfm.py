7405621f03615e802e1413b8fc525f89ee12154d
sentriz <senan.f.b.kelly+github@gmail.com>
2017-06-30 14:38:11 +0100

    format lastfm plugin

--- plugins/lastfm.py
+++ plugins/lastfm.py
@@ -4,12 +4,12 @@ from steelybot import config
 
 COMMAND = '.np'
 USERDB = TinyDB('../lastfm.json')
-user = Query()
+USER = Query()
 
 def get_np(apikey, user):
     base = "http://ws.audioscrobbler.com/2.0/"
     url = "?method=user.getrecenttracks&user={}&api_key={}&limit=2&format=json".format(user, apikey)
-    res = requests.get(base+url)
+    res = requests.get(base + url)
     return res.json()["recenttracks"]["track"][0]
 
 def extract_song(user):
@@ -23,24 +23,24 @@ def extract_song(user):
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    m = message.split()
+    message = message.split()
     if not message:
-        s = USERDB.search(user.fb_id == author_id)
-        if len(s) != 0:
-            lastfm_name = s[0]['lastfm']
+        results = USERDB.search(USER.fb_id == author_id)
+        if len(results) != 0:
+            lastfm_name = results[0]['lastfm']
             bot.sendMessage(extract_song(lastfm_name), thread_id=thread_id, thread_type=thread_type)
         else:
             bot.sendMessage('include username please or use .np set', thread_id=thread_id, thread_type=thread_type)
         return
 
-    elif m[0] == 'set' and len(m) == 2:
-        if len(USERDB.search(user.fb_id == author_id)) == 0:
+    elif message[0] == 'set' and len(message) == 2:
+        if len(USERDB.search(USER.fb_id == author_id)) == 0:
             USERDB.insert({"fb_id" : author_id, "lastfm" : m[1]})
             bot.sendMessage('good egg', thread_id=thread_id, thread_type=thread_type)
         else:
-            USERDB.update({"lastfm" : m[1]}, user.fb_id == author_id)
+            USERDB.update({"lastfm" : m[1]}, USER.fb_id == author_id)
             bot.sendMessage('updated egg', thread_id=thread_id, thread_type=thread_type)
         return
-    else: 
+    else:
         bot.sendMessage(extract_song(message), thread_id=thread_id, thread_type=thread_type)
         return
