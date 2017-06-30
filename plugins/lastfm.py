c9af4883fabdce56ee27ddd5154c4aa6bbefcc28
alex <Alex McGrath Kraak>
2017-07-01 00:11:01 +0100

    add debug tags

--- plugins/lastfm.py
+++ plugins/lastfm.py
@@ -22,6 +22,7 @@ def get_tags(artist, track):
               'method' : 'track.getTags'}
     res = requests.get(base, parmas=payload).json()
     for tag in res['tags']['tag']:
+        print(tag['name'])
         yield tag['name']
 
 
