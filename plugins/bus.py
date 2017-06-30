0aefd99b19b81dc759a0c3351cba5fb4aab47f69
sentriz <senan.f.b.kelly+github@gmail.com>
2017-06-30 17:29:21 +0100

    format bus.py

--- plugins/bus.py
+++ plugins/bus.py
@@ -1,45 +1,38 @@
 import json
 import requests
 
-COMMAND = '.dbus'
 
+COMMAND = '.dbus'
 BASE_URL = "http://data.dublinked.ie/cgi-bin/rtpi/"
+
+
 def get_stop_routes(stop_id):
-  url = BASE_URL + "busstopinformation?stopid={}&format=json".format(stop_id)
-  res = requests.get(url).json()
-  routes = []
-  for r in res["results"]:
-    for ops in r['operators']:
-      routes.extend(ops['routes'])
-  return routes
+    url = BASE_URL + "busstopinformation?stopid={}&format=json".format(stop_id)
+    response = requests.get(url).json()
+    for result in response["results"]:
+        for operator in result['operators']:
+            yield from operator['routes'])
+
 
 def next_bus_realtime(stop_id, routes):
-  url = BASE_URL + "realtimebusinformation?stopid={}&format=json".format(stop_id)
-  base_str_format = "{} should arrive in {} mins, heading to {}\n\n"
-  res = requests.get(url).json()
-  done = []
-  busses = ""
-  try: 
-    for arrival in res['results']:
-      if arrival['route'] not in done:
-        done.append(arrival['route'])
-        busses = busses + base_str_format.format(arrival['route'],
-                                                 arrival['duetime'],
-                                                 arrival['destination'])
-    return busses
-  except (TypeError, KeyError):
-    return "no results"
-    
+    url = BASE_URL + "realtimebusinformation?stopid={}&format=json".format(stop_id)
+    base_str_format = "{} should arrive in {} mins, heading to {}\n\n"
+    response = requests.get(url).json()
+    done = []
+    for arrival in response['results']:
+        if arrival['route'] not done:
+            continue
+            done.append(arrival['route'])
+            yield base_str_format.format(arrival['route'],
+                                             arrival['duetime'],
+                                             arrival['destination'])
 
-  
-  
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-  if not message:
-    bot.sendMessage("please include a stop number", thread_id=thread_id, thread_type=thread_type)
-  else:
+    if not message:
+        bot.sendMessage("please include a stop number", thread_id=thread_id, thread_type=thread_type)
+        return
     try:
-      bot.sendMessage(next_bus_realtime(message, get_stop_routes(message)), thread_id=thread_id, thread_type=thread_type)
+        bot.sendMessage('\n'.join(next_bus_realtime(message, get_stop_routes(message))), thread_id=thread_id, thread_type=thread_type)
     except requests.exceptions.RequestException:
-      bot.sendMessage("error retrieving results", thread_id=thread_id, thread_type=thread_type)
-    
+        bot.sendMessage("error retrieving results", thread_id=thread_id, thread_type=thread_type)
