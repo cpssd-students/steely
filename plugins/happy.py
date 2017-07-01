54380c81c257df3e655fd0a0cf25072ab9ba319b
alex <Alex McGrath Kraak>
2017-07-01 23:08:35 +0100

    happy bot, happy chat

---
+++ plugins/happy.py
@@ -0,0 +1,5 @@
+COMMAND = 'good bot'
+
+def main(bot, author_id, message, thread_id, thread_type, **kwargs):
+    bot.sendMessage('thanks!',
+                    thread_id=thread_id, thread_type=thread_type)
