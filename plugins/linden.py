467f52cfdf18f799a9717207087ff16aacd3b609
CianLR <cian.ruane1@gmail.com>
2017-07-01 12:56:30 +0100

    Refactor linden plugin

--- plugins/linden.py
+++ plugins/linden.py
@@ -15,6 +15,7 @@ def user_from_name(name, list):
         if user.first_name == name:
             return user
 
+
 def list_users(bot, thread_id):
     group = bot.fetchGroupInfo(thread_id)[thread_id]
     user_ids = group.participants
@@ -36,55 +37,68 @@ def get_balance(user_id):
     return matching_users['lindens']
 
 
+def give_cmd(bot, message_parts, author_id, thread_id, thread_type):
+    users = list_users(bot, thread_id)
+    if not message_parts[-1].isdigit():
+        bot.sendMessage('please enter a valid amount',
+                        thread_id=thread_id, thread_type=thread_type)
+        return
+    reciever_name, amount = message_parts[0].lstrip("@"), int(message_parts[-1])
+    reciever_model = user_from_name(reciever_name, users)
+    reciever = USERDB.get(USER.id == reciever_model.uid)
+    sender_model = bot.fetchUserInfo(author_id)[author_id]
+    sender = USERDB.get(USER.id == author_id)
+    if sender_model.uid == reciever_model.uid:
+        bot.sendMessage('no', thread_id=thread_id, thread_type=thread_type)
+        return
+    if not reciever:
+        reciever = create_user(reciever_model.uid, reciever_model.first_name)
+    if not sender:
+        sender = create_user(author_id, sender_model.first_name)
+    new_sender_balance = sender['lindens'] - amount
+    if new_sender_balance < 0:
+        bot.sendMessage('you\'d be broke boyo', thread_id=thread_id, thread_type=thread_type)
+        return
+    USERDB.update({'lindens': new_sender_balance}, USER.id == author_id)
+    USERDB.update({'lindens': reciever['lindens'] + amount}, USER.id == reciever_model.uid)
+    bot.sendMessage('you gave {} {} Linden Dollars™'.format(reciever_name, amount),
+                    thread_id=thread_id, thread_type=thread_type)
+    return
+
+
+def table_cmd(bot, message_parts, author_id, thread_id, thread_type):
+    max_lindens = len(str(max(user['lindens'] for user in USERDB.all())))
+    max_name = len(max((user['first_name'] for user in USERDB.all()), key=len))
+    string = '```'
+    for user in sorted(USERDB.all(), key=lambda user: user['lindens'], reverse=True):
+        string += '\n{name:<{max_name}} {lindens:>{max_lindens}}'.format(name=user['first_name'],
+                                                                         lindens=user['lindens'],
+                                                                         max_name=max_name,
+                                                                         max_lindens=max_lindens)
+    string += '\n```'
+    bot.sendMessage(string, thread_id=thread_id, thread_type=thread_type)
+
+
+def invalid_cmd(bot, message_parts, author_id, thread_id, thread_type):
+    bot.sendMessage("Invalid subcommand", thread_id=thread_id, thread_type=thread_type)
+
+
+SUBCOMMANDS = {
+    'give': give_cmd,
+    'send': give_cmd,
+    'table': table_cmd,
+}
+
+
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
     # .linden
     if not message:
         bot.sendMessage('you have {} Linden Dollars™'.format(get_balance(author_id)),
-            thread_id=thread_id, thread_type=thread_type)
+                        thread_id=thread_id, thread_type=thread_type)
         return
 
-    # .linden give <user> <amt>
-    message_split = message.split()
-    users = list_users(bot, thread_id)
-    if message_split[0] in ('give', 'send') and len(message_split) > 2:
-        if not message_split[-1].isdigit():
-            bot.sendMessage('please enter a valid amount', thread_id=thread_id, thread_type=thread_type)
-            return
-        reciever_name, amount = message_split[1].lstrip("@"), int(message_split[-1])
-        reciever_model = user_from_name(reciever_name, users)
-        reciever = USERDB.get(USER.id == reciever_model.uid)
-        sender_model = bot.fetchUserInfo(author_id)[author_id]
-        sender = USERDB.get(USER.id == author_id)
-        if sender_model.uid == reciever_model.uid:
-            bot.sendMessage('no', thread_id=thread_id, thread_type=thread_type)
-            return
-        if not reciever:
-            reciever = create_user(reciever_model.uid, reciever_model.first_name)
-        if not sender:
-            sender = create_user(author_id, sender_model.first_name)
-        new_sender_balance = sender['lindens'] - amount
-        if new_sender_balance < 0:
-            bot.sendMessage('you\'d be broke boyo', thread_id=thread_id, thread_type=thread_type)
-            return
-        USERDB.update({'lindens': new_sender_balance}, USER.id == author_id)
-        USERDB.update({'lindens': reciever['lindens'] + amount}, USER.id == reciever_model.uid)
-        bot.sendMessage('you gave {} {} Linden Dollars™'.format(reciever_name, amount),
-            thread_id=thread_id, thread_type=thread_type)
-        return
-
-    # .linden table
-    if message_split[0] in ('table', 'list', 'stats') and len(message_split) == 1:
-        max_lindens = len(str(max(user['lindens'] for user in USERDB.all())))
-        max_name = len(max((user['first_name'] for user in USERDB.all()), key=len))
-        string = '```'
-        for user in sorted(USERDB.all(), key=lambda user: user['lindens'], reverse=True):
-            string += '\n{name:<{max_name}} {lindens:>{max_lindens}}'.format(name=user['first_name'],
-                                                                             lindens=user['lindens'],
-                                                                             max_name=max_name,
-                                                                             max_lindens=max_lindens)
-        string += '\n```'
-        bot.sendMessage(string, thread_id=thread_id, thread_type=thread_type)
-
+    subcommand, *args = message.split()
+    SUBCOMMANDS.setdefault(subcommand, invalid_cmd)(bot, args, author_id, thread_id, thread_type)
 
 if __name__ == "__main__":
     # tests
