5504932c5251bd95e03f6fae9cb086367898ab64
sentriz <senan.f.b.kelly+github@gmail.com>
2017-07-02 21:28:15 +0100

    format ht plugin

--- plugins/ht.py
+++ plugins/ht.py
@@ -1,12 +1,13 @@
 import random
 
+
 COMMAND = '.ht'
+HEADS_IMG = "http://random-ize.com/coin-flip/us-quarter/us-quarter-front.jpg"
+TAILS_IMG = "http://random-ize.com/coin-flip/us-quarter/us-quarter-back.jpg"
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    heads = "http://random-ize.com/coin-flip/us-quarter/us-quarter-front.jpg"
-    tails = "http://random-ize.com/coin-flip/us-quarter/us-quarter-back.jpg"
-    bot.sendRemoteImage(heads if random.randint(0,1) == 0 else tails,
+    bot.sendRemoteImage(HEADS_IMG if random.randint(0, 1) else TAILS_IMG,
                         message=None,
                         thread_id=thread_id,
                         thread_type=thread_type)
