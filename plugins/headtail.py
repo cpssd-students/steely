a49245f0905fda013fb21cb1f2d769ee8da37200
Edward Dowling <EdwardDowling@users.noreply.github.com>
2017-07-02 20:46:03 +0100

    Update headtail.py

--- plugins/headtail.py
+++ plugins/headtail.py
@@ -6,4 +6,6 @@ COMMAND = '.ht'
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    bot.sendMessage(random.choice(['Heads', Tails']), thread_id=thread_id, thread_type=thread_type)
+    bot.sendMessage(random.choice(['http://random-ize.com/coin-flip/us-quarter/us-quarter-front.jpg',
+                                   'http://random-ize.com/coin-flip/us-quarter/us-quarter-back.jpg']),
+                    thread_id=thread_id, thread_type=thread_type)
