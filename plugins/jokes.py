e46c1a58742131f52006a7c0e6e853d5e6224efb
Edward Dowling <EdwardDowling@users.noreply.github.com>
2017-07-02 21:21:51 +0100

    Update jokes.py

--- plugins/jokes.py
+++ plugins/jokes.py
@@ -1,8 +1,8 @@
 import requests
-
+from steelybot import config
 
 COMMAND = '.joke'
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    r = requests.get('http://webknox.com/api/jokes/oneLiner?apiKey=bejjacgahiubszovjbsszofuegekaws')
+    r = requests.get('http://webknox.com/api/jokes/oneLiner?apiKey=' + config.JOKES_API_KEY)
     bot.sendMessage(r.json()['text'], thread_id=thread_id, thread_type=thread_type)
