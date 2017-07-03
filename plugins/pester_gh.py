393fe7276bf3a4b5abd30b8075331f0d9588c861
alex <Alex McGrath Kraak>
2017-07-03 13:24:53 +0100

    add a feature boi

---
+++ plugins/pester_gh.py
@@ -0,0 +1,9 @@
+import requests
+
+COMMAND = '.steelygh'
+
+def main(bot, author_id, message, thread_id, thread_type, **kwargs):
+    pulls = requests.get('https://api.github.com/repos/sentriz/steely/pulls').json()
+    bot.sendMessage('Senan, you have {} fucken pr's to merge'.format(len(pulls)),
+                    thread_id=thread_id, thread_type=thread_type)
+
