50b27909ad0c80ee2f0af5bb2ae2718d4f9e0fa6
Senan Kelly <senan.f.b.kelly+github@gmail.com>
2017-07-06 13:35:50 +0100

    Update xkcd.py

--- plugins/xkcd.py
+++ plugins/xkcd.py
@@ -1,10 +1,11 @@
-# sendRemoteImage(image_url, message=None, thread_id=None, thread_type=ThreadType.USER)
 import requests
+
 
 COMMAND = '.xkcd'
 
+
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    r = requests.get("https://xkcd.com/info.0.json")
-    img = r.json()['img']
-    msg = r.json()['alt']   
-    bot.sendRemoteImage(img, message=msg, thread_id=thread_id, thread_type=thread_type)
+    response = requests.get("https://xkcd.com/info.0.json")
+    image = response.json()['img']
+    message = response.json()['alt']   
+    bot.sendRemoteImage(image, message=message, thread_id=thread_id, thread_type=thread_type)
