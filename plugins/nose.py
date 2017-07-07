5f14f18364a03ece7a7f7df1e1a66f0029915ea9
sentriz <senan.f.b.kelly+github@gmail.com>
2017-07-07 22:06:22 +0100

    add nose plugin

---
+++ plugins/nose.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+import random
+
+
+COMMAND = '.nose'
+MODIFIERS = " 🏻🏼🏽🏾🏿"
+NOSE = "👃"
+
+
+def main(bot, author_id, message, thread_id, thread_type, **kwargs):
+    noses = "".join(NOSE + mod for mod in MODIFIERS) + "\n"
+    rev_noses = "".join(NOSE + mod for mod in MODIFIERS[::-1]) + "\n"
+    bot.sendMessage((noses + rev_noses) * random.randint(1, 3),
+            thread_id=thread_id, thread_type=thread_type)
