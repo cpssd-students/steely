72ced938d1639fbc294a765434f07ca1bfa0fe10
Aaron Delaney <aaron.delaney29@mail.dcu.ie>
2017-07-10 10:10:39 +0100

    Add roll.py
    Dunno if this works with the bot, but the other code works (although i did write most of it on github so i'm not sure of that either)

---
+++ plugins/roll.py
@@ -0,0 +1,44 @@
+"""
+Example:
+
+.roll 2d20 d20 1d100 ...
+"""
+import random
+
+COMMAND = '.roll'
+NLIMIT, RLIMIT = 10000, 10000
+
+def roll(n, r):
+    """Returns a formatted string of n roll results between 1 and r""" 
+    try:
+        n = int(n)
+        r = int(r)
+    except ValueError:
+        return None
+    if n > NLIMIT or r > RLIMIT:
+        return None
+    s = ','.join([str(random.randint(1, r)) for i in range(0, n)])
+    return '{}d{}:\t{}'.format(n, r, s)
+
+def parse_roll(r):
+    """Take a string that should look like: NdR and parse it"""
+    r = r.split('d')
+    if len(r) > 2:
+        return None
+    elif len(r) == 1:
+        return roll(1, r[0])
+    if len(r[0]) == 0:
+        return roll(1, r[1])
+    return roll(r[0], r[1]) 
+            
+def dorolls(s):
+    rolls = s[1:].split(' ')
+    out = []
+    for r in rolls:
+        o = parse_roll(r)
+        if o is not None:
+            out.append(o)
+    return '\n'.join(out)
+
+def main(bot, author_id, message, thread_id, thread_type, **kwargs):
+    bot.sendMessage(dorolls(message), thread_id=thread_id, thread_type=thread_type)
