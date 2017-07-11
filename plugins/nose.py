ce9385a88b5fc612d3fc442019bf7a73c3264046
sentriz <senan.f.b.kelly+github@gmail.com>
2017-07-11 14:32:33 +0100

    remove nose space

--- plugins/nose.py
+++ plugins/nose.py
@@ -5,12 +5,12 @@ import random
 
 
 COMMAND = '.nose'
-MODIFIERS = " 🏻🏼🏽🏾🏿"
+MODIFIERS = "🏻🏼🏽🏾🏿"
 NOSE = "👃"
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    noses = "".join(NOSE + mod for mod in MODIFIERS) + "\n"
-    rev_noses = "".join(NOSE + mod for mod in MODIFIERS[::-1]) + "\n"
+    noses = NOSE + "".join(NOSE + mod for mod in MODIFIERS) + "\n"
+    rev_noses = "".join(NOSE + mod for mod in MODIFIERS[::-1]) + NOSE + "\n"
     bot.sendMessage((noses + rev_noses) * random.randint(1, 3),
             thread_id=thread_id, thread_type=thread_type)
