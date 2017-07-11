1d4aa8c12114901d99f619fdc27604b54ae98925
sentriz <senan.f.b.kelly+github@gmail.com>
2017-07-11 12:21:52 +0100

    use unicode widechars

--- plugins/vapor.py
+++ plugins/vapor.py
@@ -1,7 +1,12 @@
 COMMAND = '.vapor'
 
+
 def vapor(string):
-    return ' '.join(string)
+    normal = ' 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~'
+    wide = '　０１２３４５６７８９ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ！゛＃＄％＆（）＊＋、ー。／：；〈＝〉？＠［\\］＾＿‘｛｜｝～'
+    vape_trans = str.maketrans(normal, wide)
+    return string.translate(vape_trans)
+
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
     message = bot.fetchThreadMessages(thread_id=thread_id, limit=2)[1]
