08ef542eb09d0eaafca08da0881845b0a4484de0
sentriz <senan.f.b.kelly+github@gmail.com>
2017-07-11 12:58:42 +0100

    add backticks toroll

--- plugins/roll.py
+++ plugins/roll.py
@@ -6,11 +6,11 @@ Example:
 import random
 
 COMMAND = '.roll'
-NLIMIT, RLIMIT = 999, 999 
+NLIMIT, RLIMIT = 999, 999
 
 
 def roll(n, r):
-    """Returns a formatted string of n roll results between 1 and r""" 
+    """Returns a formatted string of n roll results between 1 and r"""
     try:
         n = int(n)
         r = int(r)
@@ -19,7 +19,7 @@ def roll(n, r):
     if n > NLIMIT or r > RLIMIT:
         return None
     s = ' '.join([str(random.randint(1, r)) for i in range(0, n)])
-    return '{:<8}| {}'.format(str(n) + 'd' + str(r), s)
+    return '{:<7}| {}'.format(str(n) + 'd' + str(r), s)
 
 def parse_roll(r):
     """Take a string that should look like: NdR and parse it"""
@@ -30,8 +30,8 @@ def parse_roll(r):
         return roll(1, r[0])
     if len(r[0]) == 0:
         return roll(1, r[1])
-    return roll(r[0], r[1]) 
-            
+    return roll(r[0], r[1])
+
 def dorolls(s):
     rolls = s.split(' ')
     if len(rolls) > 25:
@@ -43,7 +43,7 @@ def dorolls(s):
             out.append(o)
     if len(out) == 0:
         return 'No valid rolls scrub'
-    return '\n'.join(out)
+    return "```" + '\n'.join(out) + "```"
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
     bot.sendMessage(dorolls(message), thread_id=thread_id, thread_type=thread_type)
