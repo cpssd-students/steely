1b7fb11b3b89d86daa08d9f8963b39eba6f07fb5
sentriz <senan.f.b.kelly+github@gmail.com>
2017-07-14 18:32:38 +0100

    issh

--- plugins/angry.py
+++ plugins/angry.py
@@ -1,10 +1,21 @@
 import random
-from fbchat import MessageReaction
+from fbchat import MessageReaction, EmojiSize
 
 
 COMMAND = None
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    if random.random() < 0.05:
-        bot.reactToMessage(kwargs['mid'], MessageReaction.ANGRY)
+    message = message.lower()
+    if random.random() < 0.001:
+            bot.sendEmoji("😠", EmojiSize.LARGE, thread_id, thread_type)
+            return
+    if any(word in message for word in ("tayne", "reed", "bot", "steely")) \
+            and random.random() < 0.65:
+        if random.random() < 0.7:
+            bot.reactToMessage(kwargs['mid'], MessageReaction.ANGRY)
+        else:
+            bot.sendEmoji("😠", EmojiSize.LARGE, thread_id, thread_type)
+    elif "cans" in message:
+        bot.reactToMessage(kwargs['mid'], MessageReaction.LOVE)
+
