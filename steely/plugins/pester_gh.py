f8bb37354bf366b17aa84f30ed5cfc1d0e2f9737
sentriz <senan.f.b.kelly+github@gmail.com>
2017-07-18 02:07:06 +0100

    add url to pester

--- steely/plugins/pester_gh.py
+++ steely/plugins/pester_gh.py
@@ -9,5 +9,5 @@ COMMAND = '.steelygh'
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
     pulls = requests.get('https://api.github.com/repos/sentriz/steely/pulls').json()
-    bot.sendMessage('Senan, you have {} fucken pr\'s to merge'.format(len(pulls)),
+    bot.sendMessage('Senan, you have {} fucken pr\'s to merge\nhttps://github.com/sentriz/steely/pulls'.format(len(pulls)),
                     thread_id=thread_id, thread_type=thread_type)
