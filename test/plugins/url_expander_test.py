356144316f66f033fff79248223c71f4a88b7805
sentriz <senan.f.b.kelly+github@gmail.com>
2017-08-23 02:02:45 +0100

    clean up url expander test

--- test/plugins/url_expander_test.py
+++ test/plugins/url_expander_test.py
@@ -1,20 +1,13 @@
-import unittest
+from nose.tools import *
 from steely.plugins.url import expand
 
 
-class UrlExpansionTest(unittest.TestCase):
-
-    def test_url_expansion(self):
-        url_pairs = [
-            ('https://tinyurl.com/2tx', 'google.com'),
-            ('https://goo.gl/JLGDfs', 'facebook.com'),
-            ('http://bit.ly/1cd2u0N', 'sourmath.com'),
-        ]
-        for short_url, expected_substring in url_pairs:
-            expanded_url = expand(short_url)
-            self.assertIn(expected_substring, expanded_url)
-
-
-if __name__ == '__main__':
-    unittest.main()
-
+def test_url_expansion():
+    url_pairs = (
+        ('https://tinyurl.com/2tx', 'google.com'),
+        ('https://goo.gl/JLGDfs', 'facebook.com'),
+        ('http://bit.ly/1cd2u0N', 'sourmath.com'),
+    )
+    for short_url, expected_substring in url_pairs:
+        expanded_url = expand(short_url)
+        yield assert_in, expected_substring, expanded_url
