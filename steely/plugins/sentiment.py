852391362b73e300115652ce2710debd8c5b978f
sentriz <senan.f.b.kelly+github@gmail.com>
2017-08-24 00:51:28 +0100

    dry

--- steely/plugins/sentiment.py
+++ steely/plugins/sentiment.py
@@ -17,9 +17,9 @@ def get_sentiment(string):
     data = response.json()
     probability = data['probability']
     body =  f"The phrase you've checked is {string!r}.\n"
-    body += f"We're {probability['pos'] * 100:.2f}% sure the message is positive\n"
-    body += f"We're {probability['neg'] * 100:.2f}% sure the message is negative\n"
-    body += f"We're {probability['neutral'] * 100:.2f}% sure the message is neutral\n"
+    for feeling in FULL_STRINGS:
+        body += f"We're {probability[feeling] * 100:.2f}% sure " + \
+                f"the message is {FULL_STRINGS[feeling]}\n"
     conclusion = FULL_STRINGS[data['label']]
     body += f"I'm gonna guess this shit is {conclusion}"
     return body
@@ -29,7 +29,7 @@ def main(bot, author_id, message, thread_id, thread_type, **kwargs):
     def send_message(message):
         bot.sendMessage(message, thread_id=thread_id, thread_type=thread_type)
     last_message = bot.fetchThreadMessages(thread_id=thread_id, limit=2)[1].text
-    if len(last_message) > 300:
+    if len(last_message) > 150:
         send_message('no')
         return
     send_message(get_sentiment(last_message))
