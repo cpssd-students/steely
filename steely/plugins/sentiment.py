f0dabf826e244ae329700904d68d5ede2b3c25c1
sentriz <senan.f.b.kelly+github@gmail.com>
2017-08-24 00:21:13 +0100

    hello sentiment

--- steely/plugins/sentiment.py
+++ steely/plugins/sentiment.py
@@ -1,25 +1,35 @@
 '''.sentiment tracks user positivity or negativity in previous message for all the autismos'''
 import requests
 import json
+
+
 COMMAND = '.sentiment'
+BASE_URL = 'http://text-processing.com/api/sentiment/'
+FULL_STRINGS = {
+    'pos': 'positive',
+    'neg': 'negative',
+    'neutral': 'neutral'
+}
 
 
 def get_sentiment(string):
-	response = requests.post("http://text-processing.com/api/sentiment/", "text=" + string)
-	data = response.json()
-	body = "The phrase you've checked is '" + string + "'.\n"
-	body += "We're " + str(int(round((data['probability']['pos'] * 100), 2))) + "% sure the message is positive\n" 
-	body += "We're " + str(int(round((data['probability']['neg'] * 100), 2))) + "% sure the message is negative\n" 
-	body += "We're " + str(int(round((data['probability']['neutral'] * 100), 2))) + "% sure the message is neutral\n"
-	if str(data['label']) == "pos":
-		body += "I'm gonna guess this shit is positive."
-	elif str(data['label']) == "neg":
-		body += "I'm gonna guess this shit is negative."
-	else:
-		body += "I'm gonna guess this shit is neutral."
+    response = requests.post(BASE_URL, f'text={string}')
+    data = response.json()
+    probability = data['probability']
+    body =  f"The phrase you've checked is {string!r}.\n"
+    body += f"We're {probability['pos'] * 100:.2f}% sure the message is positive\n"
+    body += f"We're {probability['neg'] * 100:.2f}% sure the message is negative\n"
+    body += f"We're {probability['neutral'] * 100:.2f}% sure the message is neutral\n"
+    conclusion = FULL_STRINGS[data['label']]
+    body += f"I'm gonna guess this shit is {conclusion}"
+    return body
 
-	return body
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    message = bot.fetchThreadMessages(thread_id=thread_id, limit=2)[1]
-    bot.sendMessage(get_sentiment(message.text), thread_id=thread_id, thread_type=thread_type)
+    def send_message(message):
+        bot.sendMessage(message, thread_id=thread_id, thread_type=thread_type)
+    last_message = bot.fetchThreadMessages(thread_id=thread_id, limit=2)[1].text
+    if len(last_message) > 300:
+        send_message('no')
+        return
+    send_message(get_sentiment(last_message))
