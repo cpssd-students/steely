0a373f6b623e5bd934a855e8ae889f408894a898
sentriz <senan.f.b.kelly+github@gmail.com>
2017-08-24 15:21:03 +0100

    markdown the authors

--- steely/generate_credits.py
+++ steely/generate_credits.py
@@ -10,16 +10,20 @@ def get_plugin_name(plugin):
     return plugin.__name__.rstrip('.py')
 
 
-def is_multi_author(authors):
-    valid_types = (list, tuple)
-    return any(isinstance(authors, type) for type in valid_types)
-
-
 def get_plugin_author(plugin):
     author_s = plugin.__author__
     if is_multi_author(author_s):
-        return ', '.join(author_s)
-    return author_s
+        return ', '.join(map(markdown_of, author_s))
+    return markdown_of(author_s)
+
+
+def markdown_of(author):
+    return f'[{author}](https://github.com/{author}/)'
+
+
+def is_multi_author(authors):
+    valid_types = (list, tuple)
+    return any(isinstance(authors, type) for type in valid_types)
 
 
 def get_authors():
