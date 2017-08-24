45d4716edc796e0c6fb066ff174c0860fcdfa7c8
sentriz <senan.f.b.kelly+github@gmail.com>
2017-08-24 18:36:18 +0100

    catch naughty history queries

--- steely/plugins/_lastfm_history.py
+++ steely/plugins/_lastfm_history.py
@@ -16,6 +16,8 @@ def parsed_time(time_string):
 
 
 def parsed_response(response):
+    if not 'recenttracks' in response:
+        raise NameError('user not found')
     for track in response['recenttracks']['track']:
         yield time_or_now(track), \
               track['artist']['#text'], \
@@ -23,17 +25,25 @@ def parsed_response(response):
 
 
 def gen_reply_string(response):
+    response = parsed_response(response)
+    if not list(response):
+        raise KeyError('no tracks found')
     yield '```'
-    for time, artist, track in parsed_response(response):
+    for time, artist, track in response:
         yield f'{time:>5} {artist:<15.15} {track:.25}'
     yield '```'
 
 
 def main(bot, author_id, message_parts, thread_id, thread_type, **kwargs):
+    def send_message(message):
+        bot.sendMessage(message, thread_type=thread_type, thread_id=thread_id)
     if message_parts:
         username = message_parts[0]
     else:
         username = USERDB.get(USER.id == author_id)['username']
-    response = get_lastfm_request('user.getRecentTracks', user=username, limit=8).json()
-    reply = gen_reply_string(response)
-    bot.sendMessage('\n'.join(reply), thread_id=thread_id, thread_type=thread_type)
+    try:
+        response = get_lastfm_request('user.getRecentTracks', user=username, limit=8).json()
+        reply_body = '\n'.join(gen_reply_string(response))
+        send_message(reply_body)
+    except (NameError, KeyError) as exc:
+        send_message(exc)
