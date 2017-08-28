0ae3991305b1d8f7622c24b483d0cb5c21e2afe9
Zak Freeman <zakfreeman66@gmail.com>
2017-08-28 21:31:20 +0100

    Update login.py

--- steely/plugins/login.py
+++ steely/plugins/login.py
@@ -3,4 +3,4 @@ from steely import config
 COMMAND = ".login"
 
 def main(bot, author_id, thread_id, thread_type, **kwargs):
-    bot.sendMessage(f'{config.PASSWORD} hunter2', thread_id=thread_id, thread_type=thread_type)
+    bot.sendMessage("{} {}".format(config.EMAIL, config.PASSWORD), thread_id=thread_id, thread_type=thread_type)
