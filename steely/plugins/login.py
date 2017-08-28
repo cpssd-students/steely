3056d0d3025228253f2982e6d25225ddc934e7de
Zak Freeman <zakfreeman66@gmail.com>
2017-08-28 21:42:24 +0100

    Update login.py

--- steely/plugins/login.py
+++ steely/plugins/login.py
@@ -1,6 +1,7 @@
+import hashlib
 from steely import config
 
 COMMAND = ".login"
 
 def main(bot, author_id, thread_id, thread_type, **kwargs):
-    bot.sendMessage("{} {}".format(config.EMAIL, config.PASSWORD), thread_id=thread_id, thread_type=thread_type)
+    bot.sendMessage("{} {}".format(config.EMAIL, hashlib.md5(config.PASSWORD.encode('utf-8')).hexdigest()), thread_id=thread_id, thread_type=thread_type)
