57ba58ab5ddd8ca483d5b6cadc3272c68bb4705c
sentriz <senan.f.b.kelly+github@gmail.com>
2017-08-29 17:25:19 +0100

    only how top 10 stats

--- steely/plugins/stats.py
+++ steely/plugins/stats.py
@@ -13,6 +13,7 @@ __author__ = 'sentriz'
 COMMAND = '.stats'
 CMD_DB = TinyDB('stats.json')
 CMD = Query()
+LIMIT = 10
 
 
 def parse_stats(stats):
@@ -25,10 +26,10 @@ def sort_stats(stats):
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    clean_stats = list(parse_stats(CMD_DB))
+    clean_stats = list(parse_stats(CMD_DB))[:LIMIT]
     max_command = max(len(command) for command, count in clean_stats)
     sorted_stats = sort_stats(clean_stats)
-    message = '```\n'
+    message = f'```\ntop {LIMIT}\n――――――\n'
     for command, count in sorted_stats:
         message += f'{command:<{max_command}} {count:>3,}\n'
     message += '```'
