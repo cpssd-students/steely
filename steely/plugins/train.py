c997639b6dd3b3ed2265d0e8cee255b83acaa3c6
sentriz <senan.f.b.kelly+github@gmail.com>
2017-08-29 19:46:55 +0100

    add origin and destination to train

--- steely/plugins/train.py
+++ steely/plugins/train.py
@@ -9,43 +9,43 @@ get irish rail train state times
 import requests
 import re
 from operator import itemgetter
+from xml.etree import ElementTree
+
 
 
 __author__ = 'izaakf'
 COMMAND = '.train'
-BASE_URL = 'http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML?StationDesc='
+NAMESPACES = {'realtime': 'http://api.irishrail.ie/realtime/'}
+REALTIME_URL = f"{NAMESPACES['realtime']}realtime.asmx/getStationDataByNameXML"
 
 
 def get_train_times(station):
-    url = BASE_URL + station.replace(' ', '+')
-    xml = requests.get(url)
-    xml = str(xml.text)
-
-    # regex patterns
-    destination_pattern = '(?:<Destination>)(\w+)(?:<\/Destination>)'
-    duein_pattern = '(?:<Duein>)(\d+)(?:<\/Duein>)'
-    destination = re.findall(destination_pattern, xml)
-    duein = map(int, re.findall(duein_pattern, xml))
-
-    # merge duein & destination to 2d list
-    yield from sorted(zip(destination, duein), key=itemgetter(1))[:3]
+    params = {'StationDesc': station}
+    response = requests.get(REALTIME_URL, params=params).text
+    response_tree = ElementTree.fromstring(response)
+    for station in response_tree.findall('realtime:objStationData', NAMESPACES):
+        yield station.find('./realtime:Origin', NAMESPACES).text, \
+              station.find('./realtime:Destination', NAMESPACES).text, \
+              station.find('./realtime:Duein', NAMESPACES).text
 
 
 def gen_reply_string(times):
     yield "```"
-    max_destin = max(len(line[0]) for line in times)
-    max_time = max(len(str(line[1])) for line in times)
-    for destin, time in times:
-        yield "{destin:<{max_destin}} {time:>{max_time}}min".format_map(locals())
+    max_origin = max(len(line[0]) for line in times)
+    max_destin = max(len(line[1]) for line in times)
+    max_time = max(len(str(line[2])) for line in times)
+    yield f"{'origin':<{max_origin}} destination"
+    for origin, destin, time in times:
+        yield f"{origin:<{max_origin}} {destin:<{max_destin}} {time:>{max_time}}min"
     yield "```"
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
     if not message:
-        bot.sendMessage("Invalid train station", thread_id=thread_id, thread_type=thread_type)
+        bot.sendMessage("invalid train station", thread_id=thread_id, thread_type=thread_type)
         return
     try:
-        times = list(get_train_times(message))
+        times = get_train_times(message)
     except requests.exceptions.RequestException:
         bot.sendMessage("error retrieving results", thread_id=thread_id, thread_type=thread_type)
     if times:
