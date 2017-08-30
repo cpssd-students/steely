a3763bd4443b34cc89ac2105c4a7005db7b90984
sentriz <senan.f.b.kelly+github@gmail.com>
2017-08-31 00:54:05 +0100

    add markov plugin

--- steely/plugins/markov.py
+++ steely/plugins/markov.py
@@ -2,29 +2,22 @@
 
 
 import os
+import random
+import markovify
 
 
 COMMAND = None
 LOGFOLDER = 'logs'
 
 
-def looks_like_command(message):
-    identifiers = '.', '~'
-    return any(message.startswith(char) for char in identifiers)
-
-
-def make_empty_log(log_path):
-    log_dir, log_name = os.path.split(log_path)
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-    if not os.path.exists(log_path):
-        open(log_path, 'a').close()
-
-
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    if looks_like_command(message):
+    if random.random() > 0.2: # do it 20% of the time
         return
     log_path = os.path.join(LOGFOLDER, thread_type.name, thread_id)
-    make_empty_log(log_path)
-    with open(log_path, 'a') as file:
-        file.write(message + '\n')
+    with open(log_path, 'r') as file:
+        log_model = markovify.NewlineText(file.read())
+    for _ in range(10):
+        new_sentance = log_model.make_sentence()
+        if new_sentance:
+            break
+    bot.sendMessage(new_sentance, thread_id=thread_id, thread_type=thread_type)
