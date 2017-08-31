633811f79e8c654a092bbec5b7a78ca5de64bf91
Zak Freeman <zakfreeman66@gmail.com>
2017-08-31 15:44:54 +0100

    just the typos sam and not the other dumb shit i suggested

--- steely/plugins/markov.py
+++ steely/plugins/markov.py
@@ -14,7 +14,8 @@ DELAY = 120
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    if random.random() > CHANCE_OF_SEND: return
+    if random.random() > CHANCE_OF_SEND:
+        return
     time.sleep(DELAY)
     log_path = os.path.join(LOGFOLDER, thread_type.name, thread_id)
     with open(log_path, 'r') as file:
