c11e162e62beef7ae7eda8e127dbf5561abdc62d
sentriz <senan.f.b.kelly+github@gmail.com>
2017-09-01 22:15:11 +0100

    dont get limit nbefore sorting

--- steely/plugins/stats.py
+++ steely/plugins/stats.py
@@ -26,9 +26,9 @@ def sort_stats(stats):
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    clean_stats = list(parse_stats(CMD_DB))[:LIMIT]
-    max_command = max(len(command) for command, count in clean_stats)
-    sorted_stats = sort_stats(clean_stats)
+    clean_stats = list(parse_stats(CMD_DB))
+    sorted_stats = sort_stats(clean_stats)[:LIMIT]
+    max_command = max(len(command) for command, count in sorted_stats)
     message = f'```\ntop {LIMIT}\n――――――\n'
     for command, count in sorted_stats:
         message += f'{command:<{max_command}} {count:>3,}\n'
