e65d77f8943d39beb85259aa20b7f2badd5ee8ea
sentriz <senan.f.b.kelly+github@gmail.com>
2017-09-08 14:41:40 +0100

    fix clap plugin with

--- steely/plugins/clap.py
+++ steely/plugins/clap.py
@@ -4,14 +4,15 @@
 import re
 
 
-__author__ = 'sentriz'
+__author__ = ('sentriz', 'devoxel')
 COMMAND = '.clap'
 
 
 def mock(string):
-    return re.sub(r'(\w)\s(\w)', r'\1 👏 \2', string)
+    return re.sub(r'(?<=\w)\s+(?=\w)', r' 👏 ', string)
+
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
     message = bot.fetchThreadMessages(thread_id=thread_id, limit=2)[1]
-    bot.sendMessage(mock(message.text), thread_id=thread_id, thread_type=thread_type)
+    bot.sendMessage(mock(message.text.strip()), thread_id=thread_id, thread_type=thread_type)
