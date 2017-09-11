c3efbdf50591ceb69088f95bd244a5eba4e7a8f2
sentriz <senan.f.b.kelly+github@gmail.com>
2017-09-11 16:43:58 +0100

    add ca plugin

---
+++ steely/plugins/ca.py
@@ -0,0 +1,24 @@
+'''dont use this'''
+
+
+import requests
+from steely import config
+
+
+__author__ = 'sentriz'
+COMMAND = '.sendtocachat'
+CA_CHAT_ID = '1182752481782139'
+
+
+def main(bot, author_id, message, thread_id, thread_type, **kwargs):
+    def send_message(message):
+        bot.sendMessage(message, thread_id=thread_id, thread_type=thread_type)
+    if not message.strip():
+        send_message('nice try idiot')
+        return
+    if not author_id in ('100002084833976', '1640731564', '100000433265428'):
+        send_message('sorry sam, only select people can use this command.\n' \
+                     'we couldnt want poor reed getting removed from the ca chat would we?')
+        return
+    bot.sendMessage(message, thread_id=CA_CHAT_ID, thread_type=thread_type)
+    send_message('sent')
