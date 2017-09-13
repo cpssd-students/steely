f3018e76e051b65c1ac1a240c71ac7b7043362ad
Zak Freeman <zakfreeman66@gmail.com>
2017-09-13 13:53:20 +0100

    please sam

--- steely/plugins/hello.py
+++ steely/plugins/hello.py
@@ -3,4 +3,4 @@ __author__ = 'izaakf'
 COMMAND = '.hello'
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    bot.sendMessage('Hello Sam', thread_id=thread_id, thread_type=thread_type)
+    bot.sendMessage('sam please', thread_id=thread_id, thread_type=thread_type)
