e3d6f830462615161714ff5ac874584f5779ab98
sentriz <senan.f.b.kelly+github@gmail.com>
2017-09-28 15:44:01 +0100

    add leet undo

--- steely/plugins/leet.py
+++ steely/plugins/leet.py
@@ -3,15 +3,15 @@
 
 __author__ = 'sentriz'
 COMMAND = '.leet'
-
-
-def leet(string):
-    normal = "abcdefghijklmnopqrstuvwxyz"
-    leet =   "48(d3f9#|jklmn0pqr5+uvwxy2"
-    leet_trans = str.maketrans(normal, leet)
-    return string.translate(leet_trans)
+NORMAL = "abcdefghijklmnopqrstuvwxyz"
+LEET =   "48(d3f9#|jklmn0pqr5+uvwxy2"
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    message = bot.fetchThreadMessages(thread_id=thread_id, limit=2)[1]
-    bot.sendMessage(leet(message.text), thread_id=thread_id, thread_type=thread_type)
+    last_message = bot.fetchThreadMessages(thread_id=thread_id, limit=2)[1]
+    if message in ("undo", "u"):
+        leet_trans = str.maketrans(LEET, NORMAL)
+    else:
+        leet_trans = str.maketrans(NORMAL, LEET)
+    new_message = last_message.text.translate(leet_trans)
+    bot.sendMessage(new_message, thread_id=thread_id, thread_type=thread_type)
