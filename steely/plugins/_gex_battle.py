0d9a1ba0071d66aaa98154e9b04e5efbe5bddf9a
iandioch <iandioch11@gmail.com>
2017-10-01 02:18:39 +0200

    Add battle info

--- steely/plugins/_gex_battle.py
+++ steely/plugins/_gex_battle.py
@@ -1,7 +1,37 @@
+import plugins._gex_util as gex_util
 from tinydb import TinyDB, Query
 
 BATTLE_DB = TinyDB('databases/gex_battles.json')
 BATTLE = Query()
 
+def battle_info(bot, args, author_id, thread_id, thread_type):
+    if len(args) == 0:
+        bot.sendMessage('Please provide a battle ID!', thread_id=thread_id, thread_type=thread_type)
+        return
+    battle_id = args[0]
+    matching_battles = BATTLE_DB.search(BATTLE.id == battle_id)
+    if len(matching_battles) == 0:
+        bot.sendMessage('No battle found with the given ID :(', thread_id=thread_id, thread_type=thread_type)
+        return
+    battle = matching_battles[0]
+    attacker = battle['attacker']
+    defender = battle['defender']
+    bot.sendMessage(battle_id, thread_id=thread_id, thread_type=thread_type)
+
+subcommands = {
+    'info': battle_info,
+}
+
 def battle(bot, args, author_id, thread_id, thread_type):
-    print('battle started')
+    # Should be in format .gex battle <subcommand>.
+    if len(args) == 0:
+        bot.sendMessage('Should be run in form .gex battle SUBCOMMAND .', thread_id=thread_id, thread_type=thread_type)
+        return
+    subcommand = args[0]
+    if subcommand in subcommands:
+        try:
+            subcommands[subcommand](bot, args[1:], author_id, thread_id, thread_type)
+        except Exception as e:
+            bot.sendMessage('Battle error: {}'.format(e), thread_id=thread_id, thread_type=thread_type)
+    else:
+        bot.sendMessage('No such subcommand >:(', thread_id=thread_id, thread_type=thread_type)
