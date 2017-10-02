eba7824467ac556deb1324da6631a2e31344d1e7
sentriz <senan.f.b.kelly+github@gmail.com>
2017-10-02 16:58:40 +0100

    add more spam commands

---
+++ steely/plugins/middle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+'''hello'''
+import random
+
+
+__author__ = 'sentriz'
+COMMAND = '.drunk'
+
+
+def shuffle(string):
+    return ''.join(random.sample(string, len(string)))
+
+
+def shuffle_middle(string):
+    return string[0] + shuffle(string[1:-1]) + string[-1]
+
+
+def shuffle_sentance(string):
+    return " ".join(map(shuffle_middle, string.split()))
+
+
+def main(bot, author_id, message, thread_id, thread_type, **kwargs):
+    message = bot.fetchThreadMessages(thread_id=thread_id, limit=2)[1]
+    bot.sendMessage(shuffle_sentance(message.text), thread_id=thread_id, thread_type=thread_type)
+
+
+if __name__ == "__main__":
+    print(shuffle_middle("hello"))
---
+++ steely/plugins/vowel.py
@@ -0,0 +1,23 @@
+'''no'''
+import random
+
+
+__author__ = 'izaakf'
+COMMAND = '.moccc'
+
+
+def shuffle(string):
+    return ''.join(random.sample(string, len(string)))
+
+
+def vowel_switch(string):
+    vowels = "aeiou"
+    normal = vowels + vowels.upper()
+    shuffled = shuffle(vowels) + shuffle(vowels.upper())
+    shuffle_trans = str.maketrans(normal, shuffled)
+    return string.translate(shuffle_trans)
+
+
+def main(bot, author_id, message, thread_id, thread_type, **kwargs):
+    message = bot.fetchThreadMessages(thread_id=thread_id, limit=2)[1]
+    bot.sendMessage(vowel_switch(message.text), thread_id=thread_id, thread_type=thread_type)
