65cdeb187b1be79a3fe1ded7085403215790c3aa
Edward Dowling <EdwardDowling@users.noreply.github.com>
2017-10-02 17:50:35 +0100

    Update search.py

--- steely/plugins/search.py
+++ steely/plugins/search.py
@@ -11,6 +11,6 @@ def main(bot, author_id, message, thread_id, thread_type, **kwargs):
     prev_message = bot.fetchThreadMessages(thread_id=thread_id, limit=2)[1].text
     match = re.search(message, prev_message)
     if match:
-        send_message(match.group(0))
+        send_message('\n'.join([m for m in match]))
     else:
         send_message("no match idiot")
