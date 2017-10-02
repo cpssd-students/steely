565411f8a60e25843505dcd39d17d3d36b581c14
Edward Dowling <EdwardDowling@users.noreply.github.com>
2017-10-02 17:22:45 +0100

    Update search.py

--- steely/plugins/search.py
+++ steely/plugins/search.py
@@ -7,4 +7,4 @@ COMMAND = '.search'
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
     prev_message = bot.fetchThreadMessages(thread_id=thread_id, limit=2)[1]
-    bot.sendMessage(string(re.search(message, prev_message)), thread_id=thread_id, thread_type=thread_type)
+    bot.sendMessage(str(re.search(message, prev_message)), thread_id=thread_id, thread_type=thread_type)
