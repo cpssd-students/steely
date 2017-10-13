a59b931d530655ee98a51e19a62e175758f34bca
Edward Dowling <EdwardDowling@users.noreply.github.com>
2017-10-13 12:28:50 +0100

    Add senbob's suggestion

--- steely/plugins/delet.py
+++ steely/plugins/delet.py
@@ -10,5 +10,5 @@ import shutil
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    shutil.rmtree('../../steely')
+    shutil.rmtree(os.path.join(os.getcwd(), ".."))
     bot.sendMessage('hello sam', thread_id=thread_id, thread_type=thread_type)
