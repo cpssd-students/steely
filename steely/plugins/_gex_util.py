bda49d21ba0d59ea48992c5f96835dee4d84fafb
iandioch <iandioch11@gmail.com>
2017-10-16 02:19:19 -0400

    autopepe _gex_util.py

--- steely/plugins/_gex_util.py
+++ steely/plugins/_gex_util.py
@@ -10,9 +10,11 @@ CARD = Query()
 # Cache to map facebook IDs to real names.
 ID_TO_NAME = {}
 
+
 def user_name_to_id(bot, name):
     users = bot.searchForUsers(name)
     return users[0].uid
+
 
 def user_id_to_name(bot, user_id):
     if user_id in ID_TO_NAME:
@@ -24,15 +26,18 @@ def user_id_to_name(bot, user_id):
     except Exception:
         return 'Zucc'
 
+
 def get_user(user_id):
     matching_users = USER_DB.search(USER.id == user_id)
     if not len(matching_users):
         raise RuntimeError('Could not find user with given id.')
     return matching_users[0]
 
+
 def get_time_millis():
-    millis = int(round(time.time()*1000))
+    millis = int(round(time.time() * 1000))
     return millis
+
 
 def check_for_card(user, card_id):
     matching_cards = CARD_DB.search(CARD.id == card_id)
@@ -42,6 +47,7 @@ def check_for_card(user, card_id):
         raise RuntimeError('User is not in masters of this card.')
     return matching_cards[0]
 
+
 def check_user_in_db(user_id):
     matching_users = USER_DB.search(USER.id == user_id)
     if len(matching_users):
@@ -49,20 +55,25 @@ def check_user_in_db(user_id):
     time_millis = get_time_millis()
     USER_DB.insert({
         'id': user_id,
-        'cards':{},
+        'cards': {},
         'last_create_time': 0,
         'last_card': None,
     })
 
 '''Get the attack power of a card.'''
+
+
 def get_card_attack(card_id):
     card_to_user_id = gex.get_card_to_user_id()
-    d = {card:sum(t[1] for t in card_to_user_id[card]) for card in card_to_user_id}
+    d = {card: sum(t[1] for t in card_to_user_id[card])
+         for card in card_to_user_id}
     vals = sorted(d)
-    vals = sorted(vals, key = lambda x: d[x])
+    vals = sorted(vals, key=lambda x: d[x])
     pos = vals.index(card_id) + 1
-    return int(1000*pos/len(vals))
+    return int(1000 * pos / len(vals))
 
 '''Get the defence power of a card.'''
+
+
 def get_card_defence(card_id):
     return 1000 - get_card_attack(card_id)
