305e1a65794865e82edf06ca5e6fded21fb6bbaa
Senan Kelly <senan@senan.xyz>
2017-11-15 20:36:23 +0000

    Update numberwang.py

--- steely/plugins/numberwang.py
+++ steely/plugins/numberwang.py
@@ -1,31 +1,30 @@
+#!/usr/bin/env python3
+
 """
 gimme a number and see what happens
 """
 
+
 import random
 
-__author__ = 'devoxel'
-COMMAND = 'nw'
 
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
+__author__ = 'devoxel'
+COMMAND = None
+STRINGS = ["NUMBERWANG!", 
+           "Another one",
+           "Thaaaaaat's Numberwang!",
+           "Okay", "Uh huh", 
+           "WangerNumb ;)"]
+WEIGHTS = [3, 20, 2, 20, 20, 1]
+
 
 def numberwang():
-	random.choice( ["NUMBERWANG!", 
-					"Another one",
-					"Thaaaaaat's Numberwang!",
-					"Okay", "Uh huh", 
-					"WangerNumb ;)"
-					],
-					 weights = [3, 20, 2, 20, 20, 1])
+    return random.choice(STRINGS, weights=WEIGHTS)
+        
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-	if is_number(message):
-		bot.sendMessage(numberwang(), thread_id=thread_id, thread_type=thread_type)
-		return
-	s = "G'way with that sort of thing."
-    bot.sendMessage(s, thread_id=thread_id, thread_type=thread_type)
+    try:
+        float(message)
+    except ValueError:
+        return
+    bot.sendMessage(numberwang(), thread_id=thread_id, thread_type=thread_type)
