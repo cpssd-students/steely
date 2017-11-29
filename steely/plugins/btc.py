391501f0ba4be4f431cc21357dd8b13a43f609d3
sentriz <senan@senan.xyz>
2017-11-29 19:15:18 +0000

    fix the float api thing

--- steely/plugins/btc.py
+++ steely/plugins/btc.py
@@ -5,37 +5,34 @@ __author__ = ('CianLR', 'byxor')
 COMMAND = 'btc'
 
 API_URL = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-RESP_TEMPLATE = """Bitcoin Price:
-€{}
+RESP_TEMPLATE = """€{}
 ${}
-Difference since last time: {}"""
-
+difference since last time: {}"""
 LAST_EUROS = 100
 
-class BitcoinException(Exception): pass
-class BadHttpResponse(BitcoinException): pass
-class MissingFields(BitcoinException): pass
+class BitcoinException(Exception):
+    pass
+class BadHTTPResponse(BitcoinException):
+    pass
+class MissingFields(BitcoinException):
+    pass
 
 
 def get_bitcoin_rates():
-    response = requests.get(API_URL)    
-
+    response = requests.get(API_URL)
     if not response.ok:
         message = "Error getting price. Response (code {}) {}".format(
             response.status_code, response.text)
-        raise BadHttpResponse(message)
-    
-    j = response.json()
-    
-    if not _is_valid_response(j):
-        message = "Response is missing fields: {}".format(str(j))
+        raise BadHTTPResponse(message)
+    response_json = response.json()
+    if not _is_valid_response(response_json):
+        message = "Response is missing fields: {}".format(str(response_json))
         raise MissingFields(message)
-    
-    euros = float(j['bpi']['EUR']['rate'])
-    dollars = float(j['bpi']['USD']['rate'])
-    return (euros, dollars)
+    euros = response_json['bpi']['EUR']['rate_float']
+    dollars = response_json['bpi']['USD']['rate_float']
+    return euros, dollars
 
-    
+
 def _is_valid_response(resp):
     return ('bpi' in resp and
             'EUR' in resp['bpi'] and 'rate' in resp['bpi']['EUR'] and
@@ -48,7 +45,7 @@ def percentage_increase(before, after):
 
 
 def percentage_string(n):
-    return "{0:+d}%".format(n)
+    return "{:.3f}%".format(n)
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
