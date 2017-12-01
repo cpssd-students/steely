cb5495ae6f1bbd9ee4ac1799a910e21e8e14ee8d
byxor <brandnewbyxor@gmail.com>
2017-12-01 11:46:55 +0000

    Make adjustments

--- steely/plugins/responder.py
+++ steely/plugins/responder.py
@@ -9,15 +9,15 @@ RESPONSE_CHANCE = 0.2
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
 
-    UNIVERSAL_RESPONSES = set([
+    UNIVERSAL_RESPONSES = {
         "why does it matter?",
         "who cares?",
         "do you need to know?",
         "shut up",
         "ok sam",
-    ])
+    }
 
-    PERSONAL_RESPONSES = set([
+    PERSONAL_RESPONSES = {
         "i have a name, you know",
         "beep beep boop",
         "boop beep bop",
@@ -26,39 +26,39 @@ def main(bot, author_id, message, thread_id, thread_type, **kwargs):
         "you know i'm a real person, right?",
         "i'm right here",
         "stop talking about me",
-    ])
+    }
 
-    RESPONDERS = set([
+    RESPONDERS = {
         
-        new_responder("who", set([
+        new_responder("who", {
             "who do you think?",
             "who is sam?",
             "whoever you want, sunshine",
-        ]).union(UNIVERSAL_RESPONSES)),
+        }.union(UNIVERSAL_RESPONSES)),
         
-        new_responder("what", set([
+        new_responder("what", {
             "what do you think?",
             "what do you mean by 'what'?",
-        ]).union(UNIVERSAL_RESPONSES)),
+        }.union(UNIVERSAL_RESPONSES)),
 
-        new_responder("why", set([
+        new_responder("why", {
             "why do you need to know?",
             "why this? why that? stop asking questions",
             "why are traps considered to be gay?",
-        ]).union(UNIVERSAL_RESPONSES)),
+        }.union(UNIVERSAL_RESPONSES)),
 
-        new_responder("how", set([
+        new_responder("how", {
             "how long is a piece of string?",
             "How Can Our Eyes Be Real If Mirrors Aren't Real?",
-        ]).union(UNIVERSAL_RESPONSES)),
+        }.union(UNIVERSAL_RESPONSES)),
 
         new_responder("bot", PERSONAL_RESPONSES),
         new_responder("reed", PERSONAL_RESPONSES),
         new_responder("steely", PERSONAL_RESPONSES),
-    ])
+    }
     
     response = search_for_response(RESPONDERS, message)
-    if response != "":
+    if response:
         bot.sendMessage(message, thread_id=thread_id, thread_type=thread_type)
 
 
@@ -67,8 +67,6 @@ def new_responder(trigger_word, responses):
         chance_is_met = random.random() < RESPONSE_CHANCE
         if chance_is_met and should_respond(trigger_word, message):
             return get_response(responses)
-        else:
-            return ""
     return respond_to
 
 
@@ -83,7 +81,7 @@ def get_response(responses):
 def search_for_response(responders, message):
     for responder in responders:
         response = responder(message)
-        if response != "":
-            break
-    return response
+        if response:
+            return response
+    return None
         
