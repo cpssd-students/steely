67138d4602212b3b89c1d71eae7bd0b174affc5f
itsdoddsy <23368872+itsdoddsy@users.noreply.github.com>
2017-12-19 18:02:12 +0000

    when you nut but she keep suckin

--- steely/plugins/jizz.py
+++ steely/plugins/jizz.py
@@ -3,15 +3,15 @@
 import random
 
 __author__ = 'itsdoddsy'
-COMMAND = 'senan'
-SENAN = (
+COMMAND = 'jizz'
+jizz = (
     "https://i.imgur.com/nGboTxZ.png",
     "https://i.imgur.com/3fQvF9r.png",
 )
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    bot.sendRemoteImage(random.choice(SENAN),
+    bot.sendRemoteImage(random.choice(jizz),
                         message=None,
                         thread_id=thread_id,
                         thread_type=thread_type)
