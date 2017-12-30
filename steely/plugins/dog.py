8af6c257ff68dd1d92da8d11021977a8507260e5
sentriz <senan@senan.xyz>
2017-12-30 22:04:29 +0000

    update dog.py

--- steely/plugins/dog.py
+++ steely/plugins/dog.py
@@ -1,15 +1,16 @@
 '''doggos sure are lovely'''
 
+
 import requests
+
 
 __author__ = 'itsdoddsy'
 COMMAND = 'dog'
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    imagefromapi = requests.get("https://dog.ceo/api/breeds/image/random")
-    imageurl = imagefromapi.json()
-    bot.sendRemoteImage(imageurl['message'],
-                        message=None,
+    response = requests.get('https://dog.ceo/api/breeds/image/random')
+    json_response = response.json()
+    bot.sendRemoteImage(json_response['message'],
                         thread_id=thread_id,
                         thread_type=thread_type)
