4e4d66f4a70b86b32ba06b96d8801095e07bffdb
Senan Kelly <senan@senan.xyz>
2017-12-30 22:00:11 +0000

    Update cat.py

--- steely/plugins/cat.py
+++ steely/plugins/cat.py
@@ -3,13 +3,19 @@
 import requests
 import random
 
+
 __author__ = 'itsdoddsy'
 COMMAND = 'cat'
 FORMAT = ("png", "jpg")
 
+
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    image = requests.get("http://thecatapi.com/api/images/get?type={}".format(random.choice(FORMAT)))
-    bot.sendRemoteImage(image.url,
+    params = {
+        "type": FORMAT.choice(FORMAT)
+    }
+    response = requests.get('http://thecatapi.com/api/images/get', 
+                            params=params)
+    bot.sendRemoteImage(respose.url,
                         message=None,
                         thread_id=thread_id,
                         thread_type=thread_type)
