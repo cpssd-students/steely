0bb03fc884ffea6ad3014041c21351a83b4a8760
itsdoddsy <23368872+itsdoddsy@users.noreply.github.com>
2017-12-31 19:01:04 +0000

    steely 2.0 please

--- steely/plugins/dab.py
+++
@@ -1,7 +0,0 @@
-'''༼つ ◡_◡༽つ'''
-
-__author__ = 'xCiaraG'
-COMMAND = 'dab'
-
-def main(bot, author_id, message, thread_id, thread_type, **kwargs):
-    bot.sendMessage('༼つ ◡_◡༽つ', thread_id=thread_id, thread_type=thread_type)
