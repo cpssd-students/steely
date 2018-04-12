2e7da0398d8d794127845a7aeb3f2e5a67ded2d2
byxor <brandnewbyxor@gmail.com>
2018-04-12 13:45:30 +0100

    Join strings

--- steely/plugins/_lastfm_top.py
+++ steely/plugins/_lastfm_top.py
@@ -20,12 +20,13 @@ def main(bot, author_id, message_parts, thread_id, thread_type, **kwargs):
         username = message_parts[1]
     else:
         username = USERDB.get(USER.id == author_id)["username"]
-    artists, string = [], ""
+    artists = []
     topartsts_response = get_lastfm_request('user.getTopArtists',
             period=period, user=username, limit=8)
     artists = list(parse_top(topartsts_response))
     max_artist = max(len(artist) for artist, plays in artists)
     max_plays = max(len(str(plays)) for artists, plays in artists)
+    output_lines = []
     for artist, playcount in artists:
-        string += f"\n{artist:<{max_artist}} {playcount:>{max_plays}}"
-    bot.sendMessage(code_block(string.strip()), thread_id=thread_id, thread_type=thread_type)
+        output_lines.append(f"{artist:<{max_artist}} {playcount:>{max_plays}}")
+    bot.sendMessage(code_block("\n".join(output_lines)), thread_id=thread_id, thread_type=thread_type)
