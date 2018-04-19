22ec65f75c4e10a8160f5b76b089603c72fe3f32
iandioch <iandioch11@gmail.com>
2018-04-19 23:26:24 +0000

    Allow ~image

--- steely/plugins/tilda.py
+++ steely/plugins/tilda.py
@@ -12,6 +12,11 @@ CMD = Query()
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
     search = CMD_DB.get(CMD.cmd == message)
-    if search != None:
+    if search is None:
+        return
+    if 'text' in search:
         bot.sendMessage(search['text'],
                         thread_id=thread_id, thread_type=thread_type)
+    elif 'image' in search:
+        bot.sendRemoteImage(search['image'],
+                            thread_id=thread_id, thread_type=thread_type)
