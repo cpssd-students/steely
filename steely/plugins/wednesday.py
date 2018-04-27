953b9807e3a415999ab4f7f4ab37f26e28044f9c
Ciaran Murphy <Smurphicus@users.noreply.github.com>
2018-04-27 11:52:16 +0100

    Update wednesday.py

--- steely/plugins/wednesday.py
+++ steely/plugins/wednesday.py
@@ -7,5 +7,5 @@ COMMAND = 'wednesday'
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
     message = bot.fetchThreadMessages(thread_id=thread_id, limit=2)[1]
-    wednesday_message = message + ", my dudes"
+    wednesday_message = message.text + ", my dudes"
     bot.sendMessage(wednesday_message, thread_id=thread_id, thread_type=thread_type)
