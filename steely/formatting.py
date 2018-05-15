39747cd4b0be57701189a9a4405eed43887791b0
byxor <brandnewbyxor@gmail.com>
2018-05-15 16:00:38 +0100

    Add latex formatting

--- steely/formatting.py
+++ steely/formatting.py
@@ -1,16 +1,16 @@
-bold = lambda text: _wrap(text, '*')
-italic = lambda text: _wrap(text, '_')
-monospace = lambda text: _wrap(text, '`')
-strikethrough = lambda text: _wrap(text, '~')
-
-
-def code_block(text):
-    TAG = '```'
-    NEW_LINE = '\n'
-    return _wrap(text, f'{TAG}{NEW_LINE}', f'{NEW_LINE}{TAG}')
-
-
-def _wrap(text, prefix, suffix=None):
+def _wrap(text, prefix, suffix):
     if suffix == None:
         suffix = prefix
     return f'{prefix}{text}{suffix}'
+
+
+def _new_wrapper(prefix, suffix=None):
+    return lambda text: _wrap(text, prefix, suffix)
+
+
+bold = _new_wrapper('*')
+italic = _new_wrapper('_')
+monospace = _new_wrapper('`')
+strikethrough = _new_wrapper('~')
+latex = _new_wrapper('\\(', '\\)')
+code_block = _new_wrapper('```\n', '\n```')
